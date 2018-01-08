81439b225835a8a68d68928f4b37bee82b9edca0
otomdmt <otomadmt@icloud.com>
2018-01-08 23:48:02 +0800

    Fix proxy error due to uncaught redirecting

--- pyapi/myflaskapp.py
+++ pyapi/myflaskapp.py
@@ -2,7 +2,10 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/pyapi/')
+@app.route('/pyapi')
 def pyapi():
     return "<span style='color:red'>I am a Flask app!</span>"
 
+@app.route('/pyapi/')
+def index():
+    return "<h1>Finally got this proxy shit working right!</h1><br/><p><a href='/'>Back home</a>"
